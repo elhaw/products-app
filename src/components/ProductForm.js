06033f1ebf0738a11654e87f192de660a24251da
abdulrahman <abd.elsayed@hotmail.com>
2020-01-17 20:55:32 +0400

    initialize state in the constructor function

---
+++ src/components/ProductForm.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+
+class ProductForm extends Component {
+
+render() {
+    return(
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+}
+
+
+export default ProductForm
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react'
 class SearchComponent extends Component {
     render() {
         return (
+            <form >
                 <div className="ui column fluid action input">
                     <label htmlFor="prodcutSearch"></label>
-                    <input id = "prodcutSearch" name = "prodcutSearch"  type="text" placeholder = 'search ....' />
+                    <input id="prodcutSearch" name="prodcutSearch" type="text" placeholder='search ....' defaultValue={this.props.searchText} />
                     <div className="ui button">search</div>
                 </div>
+
+            </form>
         )
     }
 }
